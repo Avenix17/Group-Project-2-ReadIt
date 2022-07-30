8357d77de70005d023cde2e5780638b061b6ee49
chigley22 <102197101+chigley22@users.noreply.github.com>
2022-07-30 10:31:04 -0600

    routes working

--- routes/api/postRoute.js
+++ routes/api/postRoute.js
@@ -1,31 +1,32 @@
-// const router = require("express").Router();
-// const { Post } = require("../../models");
+const router = require("express").Router();
+const { Post } = require("../../models");
 
-// router.get("/", async (req, res) => {
-//   try {
-//     const postData = await Post.findAll();
-//     res.status(200).json(postData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll();
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.post("/", async (req, res) => {
-//   try {
-//     const newPostData = await Post.create({
-//       id: req.body.id,
-//       title: req.body.title,
-//       entry: req.body.entry,
-//       user_id: req.body.user_id,
-//     });
-//     res.status(200).json(newPostData);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+router.post("/", async (req, res) => {
+  try {
+    const newPostData = await Post.create({
+      id: req.body.id,
+      title: req.body.title,
+      entry: req.body.entry,
+      username: req.body.username,
+      reply_to: req.body.reply_to,
+    });
+    res.status(200).json(newPostData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // router.put("/:id", (req, res) => {});
 
 // router.delete("/:id", (req, res) => {});
 
-// module.exports = router;
+module.exports = router;
