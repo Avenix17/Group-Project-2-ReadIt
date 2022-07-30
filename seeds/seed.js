1f7afade8115da4b4534e46208050412cd2e3fa7
Avyrie Fellows <avyriefellows@gmail.com>
2022-07-29 18:38:23 -0600

    Updated seed.js and created new json for other models

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,7 +3,8 @@ const userData = require('./userData.json');
 const followData = require('./followData.json');
 const bookListData = require('./bookListData.json');
 const listItemData = require('./listItemData.json');
-const { User, Follow, BookLists, ListItems } = require('../models/');
+const journalData = require('./journalData.json');
+const { User, Follow, BookLists, ListItems, Journal, Post } = require('../models/');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -22,18 +23,33 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-    //list_items seed
-  const listitems = await ListItems.bulkCreate(listItemData, {
+  //list_item seed (needs id)
+  for (const bookList of bookLists) {
+    for (const listitem of listItemData) {
+      await ListItems.create({
+        ...listitem,
+        list_id: bookList.id
+      });
+    }
+  }
+    //journal seed
+  const journal = await Journal.bulkCreate(journalData, {
     individualHooks: true,
     returning: true,
   });
+  //post and response seed
+  const parentPost = await Post.create({
+    username: 'Sal',
+    title: 'Test top level post',
+    entry: 'This is a really cool post that will get lots of replies.',
+  });
 
-
-    //   for (const project of projectData) {
-    //     await Project.create({
-    //       ...project,
-    //       user_id: users[Math.floor(Math.random() * users.length)].id,
-    //     });
+  const responsePost = await Post.create({
+    username: 'Lernantino',
+    title: 'Eh...',
+    entry: 'Not really. Sorry dude.',
+    reply_to: parentPost.id,
+  });
 
 
   process.exit(0);
