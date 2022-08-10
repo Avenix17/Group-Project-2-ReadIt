704987ffbe4d9ebf1c8ab2104545384a2b23a537
Avyrie Fellows <avyriefellows@gmail.com>
2022-08-10 16:02:39 -0600

    added s to book cover link

--- public/js/search.js
+++ public/js/search.js
@@ -12,7 +12,7 @@ function searchBook() {
         .then(a => a.json())
         .then(response => {
             for (var i = 0; i < 10; i++) {
-                document.getElementById("search-output").innerHTML += "<h2>" + response.docs[i].title + "</h2>" + response.docs[i].author_name[0] + "<br><img src = 'http://covers.openlibrary.org/b/isbn/" + response.docs[i].isbn[0] + "-M.jpg'><br>"
+                document.getElementById("search-output").innerHTML += "<h2>" + response.docs[i].title + "</h2>" + response.docs[i].author_name[0] + "<br><img src = 'https://covers.openlibrary.org/b/isbn/" + response.docs[i].isbn[0] + "-M.jpg'><br>"
             }
         });
 };
